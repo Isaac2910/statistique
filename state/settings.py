5bfa059313aba860c8836037fce9d768f4025c66
isaac omar <isaacomar29@gmail.com>
2024-12-07 05:57:39 +0100

    v5

--- state/settings.py
+++ state/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'jazzmin',
-    'grappelli',
+    #'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
